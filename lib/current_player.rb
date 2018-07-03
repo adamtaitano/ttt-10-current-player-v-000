168aa056ec85ba1bc23a99bd290c39b9ad9e8ffd
Adam Taitano <adam.taitano@gmail.com>
2018-07-03 02:51:51 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,5 +8,13 @@ def turn_count(board)
   counter
 end
 
-def current_player
+def current_player(board)
+  player = "X"
+  if turn_count(board) % 2 == 0
+    player = "X"
+  end
+  else 
+    player = "O"
+  end
+  player
 end
